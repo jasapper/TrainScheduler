6c59b5a1f8a0fcc9c494876cc453d63eeb67e380
jasapper <jasap98@yahoo.com>
2017-12-19 22:09:12 -0500

    Base files/structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCuMdKVGlrhYeW3E0Acn-Fs4r76e3q0Crk",
+    authDomain: "bootcamp-62054.firebaseapp.com",
+    databaseURL: "https://bootcamp-62054.firebaseio.com",
+    projectId: "bootcamp-62054",
+    storageBucket: "bootcamp-62054.appspot.com",
+    messagingSenderId: "262852101163"
+  };
+  firebase.initializeApp(config);
+
+  var db = firebase.database();
